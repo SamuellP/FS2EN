0de23a569a94a34ffb80ff554088680ce3129ee6
Samuel de Paula <samuelldepaula@gmail.com>
2014-11-24 00:30:55 -0200

    FS2EN

---
+++ src/br/edu/utfpr/cm/Weka/CSV/Converter.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.Weka.CSV;
+
+import java.io.File;
+import java.io.IOException;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
+
+/**
+ *
+ * @author samuel
+ */
+public class Converter {
+    public static void converterCsvToArff(File[] input, File output) throws IOException{
+        CSVLoader csvLoader = new CSVLoader();
+        ArffSaver arffSaver = null;
+        Instances data = null;
+        String project = null;
+        File saida = null;
+        
+        for(int i=0;i<input.length;i++){
+            project = input[i].getName();
+            project = project.replaceAll("csv", "arff");
+            saida = new File(output + "/" + project);
+            
+            /** Carregando arquivo CSV **/
+            csvLoader.setSource(input[0]);
+            data = csvLoader.getDataSet();
+            
+            /** Convertendo o arquivo CSV carregado para ARFF **/
+            arffSaver = new ArffSaver();
+            arffSaver.setInstances(data);
+            arffSaver.setFile(saida);
+            //arffSaver.setDestination(output);
+            arffSaver.writeBatch();
+        }
+    }
+}
---
+++ src/br/edu/utfpr/cm/Weka/CSV/CsvFile.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.Weka.CSV;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author Samuel
+ */
+public class CsvFile {
+    public static void splitCsvFile(File input, File output) throws FileNotFoundException, IOException{
+        FileReader fr = new FileReader(input);
+        BufferedReader br = new BufferedReader(fr);
+        
+        FileWriter fw = null;
+        
+        String parameters = br.readLine();
+        parameters = parameters.replace(';', ',');
+        String line = null;
+        String project = null;
+        
+        while(br.ready()){
+            line = br.readLine();
+            line = line.replace(';', ',');
+            
+            if(project == null){
+                project = getProject(line);
+                fw = new FileWriter(output + "/" + project + ".csv");
+                fw.write(parameters + "\n");
+            }else{
+                String s = getProject(line);
+                if(s.equals(project) == false){
+                    project = s;
+                    fw.flush();
+                    fw = new FileWriter(output + "/" + project + ".csv");
+                    fw.write(parameters + "\n");
+                }
+            }
+            
+            fw.write(line + "\n");   
+        }
+        fw.flush();
+    }
+    
+    public static String getProject(String s){
+        int cont = 0;
+        String project = "";
+        
+        for(int i=0;cont < 3;i++){
+            if(s.charAt(i) == ','){
+                cont++;
+            }else{
+                if(cont == 2){
+                    project += s.charAt(i);
+                }
+            }
+        }
+        
+        return project;
+    }
+}
